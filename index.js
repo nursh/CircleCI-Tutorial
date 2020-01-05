6c2d0788c347b8b4f12e58cc3c5cb35f781546ff
nursh <nsheikhmohammed01@gmail.com>
2020-01-05 09:56:57 -0500

    Added winter route [skip ci]

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({ message: 'CircleCI is running.'})
 })
+
+app.get("/winter", (req, res) => {
+  res.json({ message: "Winter is running." });
+});
+
 app.listen(8080, () => console.log(`App is running on port:8080 🚀🚀🚀 `));
--- instructions.md
+++ instructions.md
@@ -9,3 +9,6 @@
   * write all your configuration in the config file
 * Check the steps in circleci config file and update your run command
   * update yarn to npm
+
+* if you want to skip ci, add it to your commit message
+  * git commit -am 'commit message [skip ci]'
